0be812d570160a63184ee5c89e6c9d3c63e904e1
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-10-13 23:49:05 +0100

    did messages and a few other twaks

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 const stateId = "nim-state-cookie";
 const defaultState = "Logo";
+const defaultConfig = {
+    coll : 5,
+    first: true,
+    ai   : true,
+    diff : 1,
+}
 
 function showSection(elemId){
     document.getElementById(getCookie(stateId)).style.display = 'none';
-    setCookie(stateId, elemId);
 
-    var e = document.getElementById(elemId);   
+    if(isNullOrEmpty(elemId))
+        elemId=defaultState;
+    var e = document.getElementById(elemId);  
+    setCookie(stateId, elemId);
     e.style.display = 'block';
     
 }
 
+function messageHandler(v) {
+    var wrapper = document.createElement('p');
+    wrapper.innerHTML = v;
+   
+    var msgs = document.getElementById('Messages');
+    msgs.appendChild(wrapper);
+    msgs.scrollTop = msgs.scrollHeight;
+}
+
 function onLoad() {
 
-    setCookie(stateId, defaultState);
+    //setCookie(stateId, defaultState);
     if(isNullOrEmpty(getCookie(stateId)))
         setCookie(stateId, defaultState);
     showSection(getCookie(stateId));
@@ -20,10 +37,20 @@ function onLoad() {
     document.querySelectorAll('.navbar-container a').forEach((e) => {
         e.addEventListener("click", (e) => {
             //console.log(e.target.target);
-            showSection(e.target.target);
+            showSection(e.target.getAttribute('data-target'));
         })
     });
+
+    document.getElementById('messageInput').addEventListener("keypress", (e) =>{
+        if (e.key === 'Enter') {
+            //debugger;
+            messageHandler(e.target.value);
+            e.target.value = "";
+        }
+    });
 }
+
+
 
 function logIn() {
     //debugger;
