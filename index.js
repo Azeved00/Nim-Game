5846bdf077663a0a02fa85756b8363ec877c9ec7
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-12-09 17:38:26 +0000

    defined server endpoints

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const   http = require('http'),
-        port = 6004;
+        url  = require('url'),
+        port = 8004;
 
 const server = http.createServer(function (request, response) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Olá mundo\n');
-
-
+    const parsedUrl = url.parse(request.url,true);    
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+        
+    switch(pathname){
+        case "/register":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        case "/join":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        case "/leave":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        case "/notify":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        case "/update":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        case "/ranking":
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                text:"that path is well known"
+            }))
+            break;
+        default:
+            response.writeHead(404, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                error: "The path you requested is not known"
+            }));
+            break;
+    }
 });
 
 server.listen(port);
